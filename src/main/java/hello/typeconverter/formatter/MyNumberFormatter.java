0018ad5d64b9654b15968650609851c551eb1034
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-12-28 16:14:35 +0900

    feat: Formatter 구현 및 적용
    print(), parse() 메서드를 사용해보았음.

---
+++ src/main/java/hello/typeconverter/formatter/MyNumberFormatter.java
@@ -0,0 +1,28 @@
+package hello.typeconverter.formatter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.Formatter;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+@Slf4j
+public class MyNumberFormatter implements Formatter<Number> {
+    //parse -> 문자를 숫자로
+    //Number는 Integer, Long 같은 타입의 부모 클래스임.
+    @Override
+    public Number parse(String text, Locale locale) throws ParseException {
+        log.info("text={}, locale={}" , text, locale);
+        // "1,000" -> 1000
+        NumberFormat format = NumberFormat.getInstance(locale);
+        return format.parse(text);
+    }
+    //print -> 객체를 문자로
+    @Override
+    public String print(Number object, Locale locale) {
+        log.info("object={}, locale={}", object, locale);
+        NumberFormat instance = NumberFormat.getInstance(locale);
+
+        return instance.format(object);
+    }
+}
