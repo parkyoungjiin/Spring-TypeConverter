731267fc846dfe171dac901672d52151277fafc1
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-12-28 14:35:12 +0900

    feat: Converter 적용 로직
    WebConfig에 등록한 컨버터들은 스프링에서 등록된 기본 컨트롤러 보다 우선순위가 높기에 작동된다. 기본적으로 적용되는 컨버터들이 스.

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +21,17 @@ public class HelloController {
 
     @GetMapping("/hello-v2")
     public String helloV2(@RequestParam Integer data) {
-        //RequestParam은 String을 Integer로 자동으로 타입을 변환해줌.
+        //RequestParam은 String을 Integer로 자동으로 타입을 변환해줌. (컨버터가 작동됨.)
         //ModelAttribute, PathVariable도 확인 가능.
         System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        //StringToIpPort (String -> IpPort 객체로 변환하는 컨버터가 작동됨.)
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT= " + ipPort.getPort());
         return "ok";
     }
 }
