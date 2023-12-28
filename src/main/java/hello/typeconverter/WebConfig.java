c137064e9f15e5e02696c284b9c51cf1193fb8d0
parkyoungjiin <112313165+parkyoungjiin@users.noreply.github.com>
2023-12-28 17:38:22 +0900

    feat: Formatter 적용과 애노테이션 사용 로직
    WebConfig에 MyNumberFormatter를 등록한 후 사용.
    스프링이 지원하는 @NumberFormat & @DateTimeFormat를 사용하였음.

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter;
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
 import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,10 +12,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
+        // 2가지 컨버터를 주석처리 하지 않으면 우선순위로 인해서 MyNumberFormatter가 등록되지 않음
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
+
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConverter());
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
-
+        //추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
